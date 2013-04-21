b474ae52c0c75c9a2f7b9c3fd2f66eb65b2dfec8
kbijttebier@gmail.com <kbijttebier@gmail.com@99a71388-97e5-9818-b320-a92bc0cf23d2>
2013-04-21 21:01:08 +0000

    Start graphic lib

--- trunk/tests/LCDisplay.cpp
+++ trunk/tests/LCDisplay.cpp
@@ -9,17 +9,26 @@
 #include <chrono>
 #include <thread>
 #include <glog/logging.h>
+#include <stdlib.h>
 
 
 const uint8_t RS = 0;
 const uint8_t RW = 1;
 const uint8_t E = 2;
+const uint32_t CHANGED_BIT = 0x80000000;
 
 LCDisplay::LCDisplay(I2C &i2c, uint8_t address):
 	mIO(i2c, address),
 	mPortA(0),
-	mControlBus(0)
+	mControlBus(0),
+	//mGraphicRam(nullptr),
+	mGraphicRam2(nullptr)
+
 {
+	//std::array<std::array<uint16_t,100>,100>* mGraphicRam;
+   // mGraphicRam = new std::array<std::array<uint32_t,100>,100>;
+    mGraphicRam2 = new std::array<std::array<MyGraphicWord,10>,32>;
+
 	init();
 }
 
@@ -36,15 +45,86 @@ void LCDisplay::clearDisplay()
 	std::chrono::milliseconds dura( 2 );
 	std::this_thread::sleep_for( dura );
 }
+void LCDisplay::clearGraphicDisplay()
+{
+	LOG(INFO) << "Clear Graphic Display";
+
+	for (int vert = 0; vert < 32; ++vert)
+	{
+		for (int horz = 0; horz < 10; ++horz )
+		{
+			(*mGraphicRam2)[vert][horz].mBits.reset();
+			(*mGraphicRam2)[vert][horz].mChanged = true;
+		}
+	}
+	refreshDisplay();
+}
 
 void LCDisplay::writeText(uint8_t pos, std::string text)
 {
 	writeControl(0b00000110);
 	setDDRamAddress(pos); //Fist Char position
-	for (int i = 0; i < text.length(); ++i)
+	for (unsigned int i = 0; i < text.length(); ++i)
 	{
 		writeData(text[i]);
 	}
+}
+
+void LCDisplay::point(uint8_t x, uint8_t y, bool set)
+{
+	div_t divresult;
+	divresult = div(x, 16);
+	uint myX= 0;
+	LOG(INFO) << "Write Point: Quotient: " << divresult.quot << " Remainder: " << divresult.rem;
+	myX = 15 - divresult.rem;
+	LOG(INFO) << "myX: " << myX;
+	(*mGraphicRam2)[x][divresult.quot].mBits.set(myX, set);
+	(*mGraphicRam2)[x][divresult.quot].mChanged = true;
+//	refreshDisplay();
+}
+
+void LCDisplay::HLine(uint8_t x1, uint8_t x2, uint8_t y, bool set)
+{
+	for (int i = x1; i <=x2; ++i)
+	{
+		point(i,y,set);
+	}
+
+	refreshDisplay();
+}
+
+void LCDisplay::VLine(uint8_t y1, uint8_t y2, uint8_t x, bool set)
+{
+	for (int i = y1; i <=y2; ++i)
+	{
+		point(x,i,set);
+	}
+
+	refreshDisplay();
+}
+
+void LCDisplay::testGraphic()
+{
+	std::this_thread::sleep_for( std::chrono::milliseconds ( 40) );
+
+	// Set extended instruction
+	writeControl(0b00110100);
+	std::this_thread::sleep_for( std::chrono::milliseconds ( 40) );
+
+	// Set graphic display on
+	writeControl(0b00110110);
+	clearGraphicDisplay();
+
+	(*mGraphicRam2)[0][0].mBits.set();
+	(*mGraphicRam2)[0][0].mChanged = true;
+	(*mGraphicRam2)[31][9].mBits.set();
+	(*mGraphicRam2)[31][9].mChanged = true;
+	refreshDisplay();
+
+	HLine(0,159,0, true);
+	HLine(0,159,15, true);
+
+
 }
 
 void LCDisplay::checkControlBus()
@@ -61,52 +141,55 @@ void LCDisplay::init()
 	LOG(INFO) << "Init Display";
 
 	// Wait 40ms at startup
-	std::chrono::milliseconds dura( 40 );
-	std::this_thread::sleep_for( dura );
+	std::this_thread::sleep_for( std::chrono::milliseconds (40) );
 
 	// 8 bit interface
 	writeControl(0b00110000);
-//	std::this_thread::sleep_for( dura );
 	writeControl(0b00110000);
-//	std::this_thread::sleep_for( dura );
 
 	// display on
 	writeControl(0b00001100);
-//	std::this_thread::sleep_for( dura );
 
 	// home
 	writeControl(0b00000010);
-//	std::this_thread::sleep_for( dura );
 
 	clearDisplay();
 
 	// display control
 	writeControl(0b00000110);
-//	std::this_thread::sleep_for( dura );
-
-	// SEt CGRAM Address counter
-	//writeControl(0x38);
-	setDDRamAddress(0); //Fist Char position
-
-	writeData('H');
-	writeData('e');
-	writeData('l');
-	writeData('l');
-	writeData('o');
-	writeData(' ');
-	writeData('W');
-	writeData('o');
-	writeData('r');
-	writeData('l');
-	writeData('d');
-	writeData('!');
-	writeData('!');
-
 }
 
 void LCDisplay::setDDRamAddress(uint8_t addr)
 {
 	writeControl(0x80 | addr);
+}
+void LCDisplay::setGAddress(uint8_t vertical, uint8_t horizontal)
+{
+
+	writeControl(0b10000000 | vertical);
+	writeControl(0b10000000 | horizontal);
+
+}
+
+void LCDisplay::refreshDisplay()
+{
+	LOG(INFO) << "Refresh Graphic Display";
+
+	for (int vert = 0; vert < 32; ++vert)
+	{
+		for (int horz = 0; horz < 10; ++horz )
+		{
+			if ((*mGraphicRam2)[vert][horz].mChanged)
+			{
+				setGAddress(vert, horz);
+				writeData(((*mGraphicRam2)[vert][horz].mBits.to_ulong() >> 8) & 0xFF);
+				writeData((*mGraphicRam2)[vert][horz].mBits.to_ulong() & 0xFF);
+
+				// Clear Changed Bit
+				(*mGraphicRam2)[vert][horz].mChanged = false;
+			}
+		}
+	}
 }
 
 void LCDisplay::writeData(uint8_t byte)
--- trunk/tests/LCDisplay.h
+++ trunk/tests/LCDisplay.h
@@ -12,19 +12,32 @@
 #include <stdint.h>
 #include <bitset>
 #include <string>
+#include <array>
+struct MyGraphicWord
+{
+	std::bitset<16> mBits;
+	bool mChanged;
+};
 
 class LCDisplay {
 public:
 	LCDisplay(I2C &i2c, uint8_t address);
 	virtual ~LCDisplay();
 	void clearDisplay();
+	void clearGraphicDisplay();
 	void writeText(uint8_t pos, std::string text);
-
+	void point(uint8_t x, uint8_t y, bool set);
+	void HLine(uint8_t x1, uint8_t x2, uint8_t y, bool set);
+	void VLine(uint8_t y1, uint8_t y2, uint8_t x, bool set);
+	void testGraphic();
 private:
 	void init();
 	void checkControlBus();
 
 	void setDDRamAddress(uint8_t addr);
+	void setGAddress(uint8_t horizontal, uint8_t vertical);
+
+	void refreshDisplay();
 	void writeData(uint8_t byte);
 	void writeControl(uint8_t byte);
 	uint8_t readControl();
@@ -32,8 +45,9 @@ private:
 	IOExpander mIO;
 	uint8_t mPortA;
 	std::bitset<8> mControlBus;
-	bool test;
-
+//	std::array<std::array<uint32_t,10>,32> mGraphicRam;
+	//std::array<std::array<uint32_t,100>,100>* mGraphicRam;
+	std::array<std::array<MyGraphicWord,10>,32>* mGraphicRam2;
 
 };
 
