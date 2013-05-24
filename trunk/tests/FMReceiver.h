658dd2734f11dedbe5b1e7a93a0a181a7bb10255
kbijttebier@gmail.com <kbijttebier@gmail.com@99a71388-97e5-9818-b320-a92bc0cf23d2>
2013-05-24 21:18:26 +0000

    Radio working !!

--- trunk/tests/FMReceiver.h
+++ trunk/tests/FMReceiver.h
@@ -11,17 +11,34 @@
 #include "I2C.h"
 #include <stdint.h>
 
+enum class PowerState
+{
+	POWERON,
+	POWEROFF,
+	UNKNOWN
+};
+
 class FMReceiver {
 public:
 	FMReceiver(I2C &i2c, uint8_t address);
 	virtual ~FMReceiver();
+
+	bool powerOn();
+	bool powerOff();
+	bool seekUp(int timeout);
+	bool tuneFrequency(double frequency);
 private:
 	bool init();
+
+	void debugTuningStatus();
 	bool waitForCTS();
+	bool waitForSTC();
 	bool readCTS();
 	bool readSTC();
+
 	I2C &mI2C;
 	const uint8_t mAddress;
+	PowerState mPowerState;
 };
 
 #endif /* FMRECEIVER_H_ */
