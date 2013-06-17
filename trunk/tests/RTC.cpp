47812c2e582d1c4b035f6862e5da42fad61e070f
kbijttebier@gmail.com <kbijttebier@gmail.com@99a71388-97e5-9818-b320-a92bc0cf23d2>
2013-06-17 18:50:20 +0000

    rtc read at startup

--- trunk/tests/RTC.cpp
+++ trunk/tests/RTC.cpp
@@ -10,26 +10,16 @@
 #include <iostream>
 #include <stdio.h>
 #include <glog/logging.h>
+#include <fstream>
 
-RTC::RTC(I2C &i2c):
-	mI2C(i2c)
+std::string runCmd(const std::string& cmd, bool log)
 {
-
-}
-
-RTC::~RTC()
-{
-
-}
-
-std::string readNTPStatus()
-{
-	std::string ntpqCmd("ntpq -p");
-    FILE* pipe = popen(ntpqCmd.c_str(), "r");
+    FILE* pipe = popen(cmd.c_str(), "r");
 
     if (!pipe)
     {
-    	return "ERROR";
+    	LOG(ERROR) << "Unable to run Cmd: " << cmd;
+    	return "";
     }
 
     char buffer[128];
@@ -42,12 +32,59 @@ std::string readNTPStatus()
     	}
     }
     pclose(pipe);
+    if (log)
+    {
+    	LOG(INFO) << "Run Cmd: " << cmd;
+        std::istringstream resultStream(result);
+        std::string line;
+        while (std::getline(resultStream, line))
+        {
+        	LOG(INFO) << line;
+        }
+
+    }
+
     return result;
 }
 
-void RTC::showNTPStatus()
+
+RTC::RTC(I2C &i2c):
+	mI2C(i2c)
 {
-	std::string ntpStatus =  readNTPStatus();
+	LOG(INFO) << "Checking time accuracy";
+
+	if (!ntpSynchronized())
+	{
+		LOG(INFO) << "Accessing RTC Clock";
+		std::ifstream ifile("/sys/bus/i2c/devices/1-0068");
+		if (!ifile)
+		{
+			LOG(INFO) << "DS1307 not registered, registering...";
+		  // RTC Device not registered on I2C bus
+			std::ofstream newDevice("/sys/class/i2c-adapter/i2c-1/new_device");
+			if (newDevice)
+			{
+				newDevice << "ds1307 0x68";
+			}
+			newDevice.close();
+		}
+		LOG(INFO) << "Synchronising hwclock with DS1307";
+		runCmd("hwclock -s --debug", true);
+	}
+	else
+	{
+		LOG(INFO) << "Time synchronized with ntp server";
+	}
+}
+
+RTC::~RTC()
+{
+
+}
+
+bool RTC::ntpSynchronized()
+{
+	std::string ntpStatus =  runCmd("ntpq -p", false);
     std::istringstream resultStream(ntpStatus);
     std::string line;
     while (std::getline(resultStream, line)) {
@@ -68,11 +105,31 @@ void RTC::showNTPStatus()
     	    {
 				LOG(INFO) << "Synchronised with: " << columns[0];
 				LOG(INFO) << "Delay: " << columns[7] << "msec";
+				std::istringstream input(columns[7]);
+				double delay;
+				if (!(input >> delay))
+				{
+					delay = 0;
+				}
+
+				if ((delay > 5) && (delay < 100))
+				{
+					return true;
+				}
+				else
+				{
+					return false;
+				}
+
     	    }
     	}
     }
+    return false;
+}
 
-   // LOG(INFO) << readNTPStatus();
+void RTC::showNTPStatus()
+{
+	std::string ntpStatus =  runCmd("ntpq -p", true);
 }
 
 
