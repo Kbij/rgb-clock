8ba8034303bc4714cb09ae67e4b42cdee6188a7d
kbijttebier@gmail.com <Kbij@users.noreply.github.com>
2013-06-23 16:13:30 +0000

    New directory structure

---
+++ rgbclock/lib/IOExpander.cpp
@@ -0,0 +1,139 @@
+/*
+ * IOExpander.cpp
+ *
+ *  Created on: Apr 10, 2013
+ *      Author: koen
+ */
+
+#include "IOExpander.h"
+const uint8_t DIRA  = 0x00;
+const uint8_t DIRB  = 0x01;
+
+const uint8_t GPIOA = 0x12;
+const uint8_t GPIOB = 0x13;
+
+const uint8_t GPPUA = 0x0C;
+const uint8_t GPPUB = 0x0D;
+
+
+IOExpander::IOExpander(I2C &i2c, uint8_t address) :
+			mI2C(i2c),
+			mAddress(address)
+{
+	init();
+}
+
+IOExpander::~IOExpander() {
+
+}
+bool IOExpander::writeA(uint8_t byte)
+{
+	std::vector<uint8_t> buffer;
+	buffer.push_back(GPIOA); // Register: GPIOA (BANK = 0)
+	buffer.push_back(byte);
+
+	return mI2C.writeDataSync(mAddress, buffer);
+}
+
+bool IOExpander::writeB(uint8_t byte)
+{
+	std::vector<uint8_t> buffer;
+	buffer.push_back(GPIOB); // Register: GPIOB (BANK = 0)
+	buffer.push_back(byte);
+
+	return mI2C.writeDataSync(mAddress, buffer);
+}
+
+bool IOExpander::directionA(DataDirection direction)
+{
+	std::vector<uint8_t> initBuffer;
+	if (direction == DataDirection::dirOut)
+	{
+		initBuffer.push_back(DIRA); // Register DirA
+		initBuffer.push_back(0x00); // All Output
+		mI2C.writeDataSync(mAddress, initBuffer);
+
+		initBuffer.clear();
+		initBuffer.push_back(GPPUA); // PullUp PortA
+		initBuffer.push_back(0x00); // No Pullup
+		mI2C.writeDataSync(mAddress, initBuffer);
+
+	}
+	else
+	{
+		initBuffer.push_back(DIRA); // Register DirA
+		initBuffer.push_back(0xFF); // All Input
+		mI2C.writeDataSync(mAddress, initBuffer);
+
+		initBuffer.clear();
+		initBuffer.push_back(GPPUA); // PullUp PortA
+		initBuffer.push_back(0xFF); // All Pullup
+		mI2C.writeDataSync(mAddress, initBuffer);
+	}
+	return true;
+}
+
+bool IOExpander::directionB(DataDirection direction)
+{
+	std::vector<uint8_t> initBuffer;
+	if (direction == DataDirection::dirOut)
+	{
+		initBuffer.push_back(DIRB); // Register DirB
+		initBuffer.push_back(0x00); // All Output
+		mI2C.writeDataSync(mAddress, initBuffer);
+
+		initBuffer.clear();
+		initBuffer.push_back(GPPUB); // PullUp PortB
+		initBuffer.push_back(0x00); // No Pullup
+		mI2C.writeDataSync(mAddress, initBuffer);
+
+	}
+	else
+	{
+		initBuffer.push_back(DIRB); // Register DirB
+		initBuffer.push_back(0xFF); // All Input
+		mI2C.writeDataSync(mAddress, initBuffer);
+
+		initBuffer.clear();
+		initBuffer.push_back(GPPUB); // PullUp PortB
+		initBuffer.push_back(0xFF); // All Pullup
+		mI2C.writeDataSync(mAddress, initBuffer);
+	}
+	return true;
+}
+
+bool IOExpander::readA(uint8_t& byte)
+{
+	return mI2C.readByteSync(mAddress, GPIOA, byte);
+}
+
+bool IOExpander::readB(uint8_t& byte)
+{
+	return mI2C.readByteSync(mAddress, GPIOB, byte);
+}
+
+bool IOExpander::init()
+{
+	bool result = true;
+	std::vector<uint8_t> initBuffer;
+	initBuffer.push_back(0x0A); // Register: IOCON (After POR BANK = 0)
+	/*
+	 * Bits:
+	 * 7:0: Bank
+	 * 6:0: Mirror
+	 * 5:1: SeqOp
+	 * 4:0: DisSlw
+	 * 3:0: Haen
+	 * 2:0: Odr
+	 * 1:0: IntPol
+	 * 0:0: Unimplemented
+	 */
+	initBuffer.push_back(0b00100000);
+
+	result = result && mI2C.writeDataSync(mAddress, initBuffer);
+
+	result = result & directionA(DataDirection::dirOut);
+	result = result & directionB(DataDirection::dirOut);
+
+	return result;
+}
---
+++ rgbclock/lib/LCDisplay.h
@@ -0,0 +1,84 @@
+/*
+ * LCDisplay.h
+ *
+ *  Created on: Apr 11, 2013
+ *      Author: koen
+ */
+
+#ifndef LCDISPLAY_H_
+#define LCDISPLAY_H_
+#include "I2C.h"
+#include "IOExpander.h"
+#include <stdint.h>
+#include <bitset>
+#include <string>
+#include <array>
+#include <map>
+
+
+struct MyGraphicWord
+{
+	std::bitset<16> mBits;
+	bool mChanged;
+};
+enum class FontType
+{
+	Verdana20,
+	Courier15,
+	Large9,
+	Volter,
+	Terminal8
+};
+
+struct FontInfo
+{
+	uint8_t mWidth;
+	uint8_t mHeight;
+	uint8_t mSpacing;
+	const std::vector<uint8_t>* mPointer;
+};
+
+class LCDisplay {
+public:
+	LCDisplay(I2C &i2c, uint8_t address);
+	virtual ~LCDisplay();
+	void initStandard();
+	void clearStandardDisplay();
+
+	void initGraphic();
+	void clearGraphicDisplay();
+
+	void writeText(uint8_t pos, std::string text);
+	void writeGraphicText(uint8_t col, uint8_t row, std::string text, FontType font);
+
+	void point(uint8_t x, uint8_t y, bool set);
+	void hLine(uint8_t col1, uint8_t col2, uint8_t row, bool set);
+	void vLine(uint8_t col, uint8_t row1, uint8_t row2, bool set);
+	void rectangle(uint8_t x1, uint8_t y1, uint8_t x2, uint8_t y2, bool set, bool fill);
+
+private:
+	void init();
+
+	void rawPoint(uint8_t col, uint8_t row, bool set);
+	void rawVertByte(uint8_t col, uint8_t& row, uint8_t maxRemainingRows, uint8_t byte);
+	void rawHLine(uint8_t col1, uint8_t col2, uint8_t row, bool set);
+	void rawVLine(uint8_t col, uint8_t row1, uint8_t row2, bool set);
+	void rawGraphicChar(uint8_t col, uint8_t row, uint8_t character, FontType font);
+
+	void setDDRamAddress(uint8_t addr);
+	void setGAddress(uint8_t horizontal, uint8_t vertical);
+
+	void refreshDisplay();
+	void writeData(uint8_t byte);
+	void writeControl(uint8_t byte);
+	uint8_t readControl();
+	uint8_t readData();
+	IOExpander mIO;
+	uint8_t mPortA;
+	std::bitset<8> mControlBus;
+	std::array<std::array<MyGraphicWord,10>,32>* mGraphicRam;
+	std::map<FontType, FontInfo> mFontMap;
+	std::mutex mDisplayMutex;
+};
+
+#endif /* LCDISPLAY_H_ */
