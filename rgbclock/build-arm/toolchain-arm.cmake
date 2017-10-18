f92b373cea5f10971cda597c98e029b05c5f9808
Kbij <Kbij@users.noreply.github.com>
2017-10-18 21:27:13 +0200

    toolchain file

--- rgbclock/build-arm/toolchain-arm.cmake
+++ rgbclock/build-arm/toolchain-arm.cmake
@@ -1,6 +1,6 @@
 
 #set( CMAKE_VERBOSE_MAKEFILE on )
-set( HCB_PLATFORM "arm")
+set( RGBCLOCK_PLATFORM "arm")
 
 # this one is important
 SET(CMAKE_SYSTEM_NAME Linux)
@@ -14,15 +14,11 @@ SET(CMAKE_ASM_COMPILER ${CMAKE_CURRENT_SOURCE_DIR}/toolchain/gcc-linaro-arm-linu
 SET(CMAKE_SYSTEM_PROCESSOR arm)
 
 # where is the target environment 
-SET(CMAKE_FIND_ROOT_PATH  /home/qbus/hcb/libraries/sysroot-arm)
+SET(CMAKE_FIND_ROOT_PATH  ${CMAKE_CURRENT_SOURCE_DIR}/sysroot-arm)
 
 # search for programs in the build host directories
-#SET(CMAKE_FIND_ROOT_PATH_MODE_PROGRAM NEVER)
 SET(CMAKE_FIND_ROOT_PATH_MODE_PROGRAM BOTH)
 # for libraries and headers in the target directories
-#SET(CMAKE_FIND_ROOT_PATH_MODE_LIBRARY ONLY)
 SET(CMAKE_FIND_ROOT_PATH_MODE_LIBRARY BOTH)
-
-#SET(CMAKE_FIND_ROOT_PATH_MODE_INCLUDE ONLY)
 #this fixes the problem with boost (find_package(boost)
 SET(CMAKE_FIND_ROOT_PATH_MODE_INCLUDE BOTH)
